3c36f895f32d3c43d67ca86a0c1400a55930eb9a
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-06 21:45:00 +0530

    Create temperature in cel to kelvin.java

---
+++ codekata/beginner/set10/temperature in cel to kelvin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+class sum
+{
+	public static void main (String[] args) 
+	{
+	Scanner in=new Scanner(System.in);
+	int tempC=in.nextInt();
+	int n=273;
+	int tempK=tempC+n;
+	System.out.println(tempK);
+	}
+}
