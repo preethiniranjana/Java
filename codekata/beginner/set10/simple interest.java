40c7eb7b23e1aac8a861982f1f9f339382dfaeac
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-03-06 22:45:22 +0530

    Create simple interest.java

---
+++ codekata/beginner/set10/simple interest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+class si
+{
+	public static void main (String[] args) 
+	{
+		int p,t,r,R,si;
+	Scanner in=new Scanner(System.in);
+	 p=in.nextInt();
+	 t=in.nextInt();
+	 r=in.nextInt();
+	 si=(p*t*r)/100;
+	System.out.println(si);
+}
+}
