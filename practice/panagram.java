2ea090b2418f2c2e69ad202adcffc41682a6c0b3
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-06-26 12:01:42 +0530

    Create panagram.java

---
+++ practice/panagram.java
@@ -0,0 +1,32 @@
+package panagramjava;
+
+public class All_letters
+{
+	public static boolean ispanagram(String str)
+	{
+	boolean[] arr=new boolean[26];
+	int index=0;
+	for(int i=0;i<str.length();i++)
+	{
+		if('A'<=str.charAt(i) && str.charAt(i)<='Z')
+		index=str.charAt(i)-'A';
+		else if('a'<=str.charAt(i) && str.charAt(i)<='z')
+		index=str.charAt(i)-'a';
+		arr[index]=true;
+	}
+	for(int i=0;i<=25;i++)
+		if(arr[i]==false)
+			return(false);
+	return (true);
+	}
+	public static void main(String[] args)
+	{
+	String str="The quick brown fox jumps over the lazy dog";
+	if(ispanagram(str)==true)
+		System.out.println(str+" "+"is a panagram.");
+	else
+		System.out.println(str+" "+"is not a panagram.");
+	
+	}
+
+}
