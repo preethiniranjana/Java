1e3a018ed84ebcec36f21b4d358495aea8b6610a
preethiniranjana <32997412+preethiniranjana@users.noreply.github.com>
2018-06-27 15:08:14 +0530

    Create greater_num.java

---
+++ practice/greater_num.java
@@ -0,0 +1,18 @@
+package twonums;
+
+public class Greater_num {
+public static int func(int x,int  y)
+{
+	if(x>y)
+	return x;
+	else
+		return y;
+}
+	public static void main(String[] args)
+	{
+	System.out.println(func(10,40));
+	System.out.println(func(30,10));
+
+	}
+
+}
